11561bed97694bb624be4006c80fa622f7aea2d8
Igor Lenko <mk2008@mail.ru>
2021-04-07 23:09:05 +0300

    init:02-task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,34 +1,32 @@
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
 function getCommonCharacterCount(s1, s2) {
-  const obj1 = {}
-  const obj2 = {}
+  const obj1 = {};
+  const obj2 = {};
+  const arr1 = [...s1];
+  const arr2 = [...s2];
 
-  const arr1 = [...s1].forEach(e => {
-    (!obj1[e])? obj1[e] = 1 : obj1[e] = obj1[e]+1
-  });
-  const arr2 = [...s2].forEach(e => {
-    (!obj2[e])? obj2[e] = 1 : obj2[e] = obj2[e]+1
-  });
-
-  const arrKeys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])]
-
-  return arrKeys.map(e => {
-    if(obj1[e] + obj2[e]){
-      return (obj1[e] > obj2[e])? obj2[e] : obj1[e]
+  arr1.forEach((e) => {
+    if (!obj1[e]) {
+      obj1[e] = 1;
     } else {
-      return 0
+      obj1[e] += 1;
     }
-  }).reduce((sum, elem) => sum + elem)
+  });
+  arr2.forEach((e) => {
+    if (!obj2[e]) {
+      obj2[e] = 1;
+    } else {
+      obj2[e] += 1;
+    }
+  });
+
+  const arrKeys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])];
+
+  return arrKeys.map((e) => {
+    if (obj1[e] + obj2[e]) {
+      return (obj1[e] > obj2[e]) ? obj2[e] : obj1[e];
+    }
+    return 0;
+  }).reduce((sum, elem) => sum + elem);
 }
 
 module.exports = getCommonCharacterCount;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,16 +1,12 @@
-/**
- * Given an array with heights, sort them except if the value is -1.
- *
- * @param {Array} arr
- * @return {Array}
- *
- * @example
- * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
- *
- * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
-function sortByHeight(/* arr */) {
-  // throw new Error('Not implemented');
+function sortByHeight() {
+  // let i = 0
+  // const arrSortWhithHeight = arr.filter((item) => {
+  //   return (item !== -1)? true : false
+  // }).sort((a,b) => a-b)
+
+  // return arr.map(e => {
+  //   return (e === -1)? e : e = arrSortWhithHeight[i++]
+  // })
 }
 
 module.exports = sortByHeight;
