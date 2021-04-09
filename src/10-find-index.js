d20ec8c24cbcef2fe77556ab4183c5062be068d2
Igor Lenko <mk2008@mail.ru>
2021-04-09 20:47:28 +0300

    init:11-task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,18 +1,37 @@
-/**
- * Given a sorted array, find the index of the element with the given value.
- * Time complexity should be O(logN)
- *
- * @param {Array} array
- * @param {Number} value
- * @return {Number}
- *
- * @example
- * For ([1, 2, 3], 1) should return 0
- * For ([1, 2, 3], 2) should return 1
- *
- */
-function findIndex(/* array, value */) {
-  // throw new Error('Not implemented');
+function findIndex(array, value) {
+  let count = Math.ceil(array.length / 2);
+
+  function getHalfNumber(arr) {
+    const halfLength = Math.ceil(arr.length / 2);
+    const index = halfLength - 1;
+    const num = arr[index];
+
+    if (num === value) {
+      count -= 1;
+    } else if (value > num) {
+      const newArr = arr.slice(halfLength);
+      const step = Math.ceil(newArr.length / 2);
+      count += step;
+
+      getHalfNumber(newArr);
+    } else {
+      const newArr = arr.slice(0, index);
+
+      if (newArr.length === 2) {
+        const step = newArr.length / 2 + 1;
+        count -= step;
+      } else {
+        const step = Math.ceil(newArr.length / 2);
+        count -= step;
+      }
+
+      getHalfNumber(newArr);
+    }
+  }
+
+  getHalfNumber(array);
+
+  return count;
 }
 
 module.exports = findIndex;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  // throw new Error('Not implemented');
+function encodeLine(str) {
+  const accum = [];
+  const array = str.split('');
+  let count = 1;
+
+  array.forEach((elem, i, arr) => {
+    if (elem === arr[i + 1]) {
+      count++;
+    } else {
+      if (count === 1) {
+        accum.push(elem);
+      } else {
+        accum.push(count + elem);
+      }
+      count = 1;
+    }
+  });
+
+  const string = accum.join('');
+
+  return string;
 }
 
 module.exports = encodeLine;
